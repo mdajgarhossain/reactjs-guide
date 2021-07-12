a8fe520355ee90187dceca08f4b97a5873eb4b84
mdajgarhossain <mdajgarhossain1@gmail.com>
2021-07-12 17:06:27 +0600

    Added basic css styles

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
     return (
-        <div>
-            <div>July 12, 2021</div>
-            <div>
-                <h2>Car Insurance</h2>
-                <div>$294.67</div>
-            </div>
+      <div className="expense-item">
+        <div>July 12, 2021</div>
+        <div className="expense-item__description">
+          <h2>Car Insurance</h2>
+          <div className="expense-item__price">$294.67</div>
         </div>
+      </div>
     );
 }
 
