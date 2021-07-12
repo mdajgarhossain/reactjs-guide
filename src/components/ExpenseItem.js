f757f7b67ecb3bcf107f6c8f3bfcbe4c2ff8952c
mdajgarhossain <mdajgarhossain1@gmail.com>
2021-07-12 16:07:19 +0600

    Wrote a bit more complex JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,12 @@
 function ExpenseItem() {
     return (
-        <h2>Expense Item !</h2>
+        <div>
+            <div>July 12, 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$294.67</div>
+            </div>
+        </div>
     );
 }
 
