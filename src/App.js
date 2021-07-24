6c30802f30c050b61b0e9ac7715f0937f7c0b856
mdajgarhossain <mdajgarhossain1@gmail.com>
2021-07-24 17:43:45 +0600

    Replaced regular funtion sytax using arrow function syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from "react";
 import Expenses from "./components/Expense/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -35,19 +34,12 @@ function App() {
     },
   ];
 
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "Let's get started!"),
-    React.createElement(Expenses, {items: expenses})
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses}/>
+    </div>
   );
-
-  // return (
-  //   <div>
-  //     <h2>Let's get started!</h2>
-  //     <Expenses items={expenses}/>
-  //   </div>
-  // );
 }
 
 export default App;
