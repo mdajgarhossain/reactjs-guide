4f51e4ed34525c9f19d0ac787be930ce8e0fda80
mdajgarhossain <mdajgarhossain1@gmail.com>
2021-07-27 16:47:22 +0600

    Commented other alternative approach of state updating function

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,54 +4,57 @@ import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
   // Here is multiple three state
-  // const [enteredTitle, setEnteredTitle] = useState("");
-  // const [enteredAmount, setEnteredAmount] = useState("");
-  // const [enteredDate, setEnteredDate] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
   // Using one state instead
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
 
   const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value);
+    setEnteredTitle(event.target.value);
+
     // setUserInput({
     //   ...userInput,
     //   enteredTitle: event.target.value
     // });
 
     // Note: If we schedule a lot of state updates at the same time, we could be depending on outdated or incorrect state snapshot & that's why the new approach below
-    setUserInput((prevState) => {
-      return { ...prevState, enteredTitle: event.target.value };
-    });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   };
   const amountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
-    });
+    setEnteredAmount(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
   const dateChangeHandler = (event) => {
-    // setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
-    });
+    setEnteredDate(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
   };
 
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
-          <pre>{userInput.enteredTitle}</pre>
+          <pre>{enteredTitle}</pre>
           <label>Title</label>
           <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
-          <pre>{userInput.enteredAmount}</pre>
+          <pre>{enteredAmount}</pre>
           <label>Amount</label>
           <input
             type="number"
@@ -61,7 +64,7 @@ const ExpenseForm = () => {
           />
         </div>
         <div className="new-expense__control">
-          <pre>{userInput.enteredDate}</pre>
+          <pre>{enteredDate}</pre>
           <label>Date</label>
           <input
             type="date"
